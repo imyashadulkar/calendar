d238221b09f2676c3c85a5c6809ab727f151ea87
Yash Adulkar <yash151099@gmail.com>
2021-04-13 13:30:36 +0530

    Initial commit

---
+++ lib/calendar.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+
+class Calendar extends StatefulWidget {
+  @override
+  _CalendarState createState() => _CalendarState();
+}
+
+class _CalendarState extends State<Calendar> {
+  CalendarFormat format = CalendarFormat.month;
+  DateTime selectedDay = DateTime.now();
+  DateTime focusedDay = DateTime.now();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("ESTech Calendar"),
+        centerTitle: true,
+      ),
+      body: TableCalendar(
+        focusedDay: selectedDay,
+        firstDay: DateTime(1990),
+        lastDay: DateTime(2050),
+        calendarFormat: format,
+        onFormatChanged: (CalendarFormat _format) {
+          setState(() {
+            format = _format;
+          });
+        },
+        startingDayOfWeek: StartingDayOfWeek.sunday,
+        daysOfWeekVisible: true,
+
+        //Day Changed
+        onDaySelected: (DateTime selectDay, DateTime focusDay) {
+          setState(() {
+            selectedDay = selectDay;
+            focusedDay = focusDay;
+          });
+          print(focusedDay);
+        },
+        selectedDayPredicate: (DateTime date) {
+          return isSameDay(selectedDay, date);
+        },
+
+        //To style the Calendar
+        calendarStyle: CalendarStyle(
+          isTodayHighlighted: true,
+          selectedDecoration: BoxDecoration(
+            color: Colors.blue,
+            shape: BoxShape.rectangle,
+            borderRadius: BorderRadius.circular(5.0),
+          ),
+          selectedTextStyle: TextStyle(color: Colors.white),
+          todayDecoration: BoxDecoration(
+            color: Colors.purpleAccent,
+            shape: BoxShape.rectangle,
+            borderRadius: BorderRadius.circular(5.0),
+          ),
+        ),
+        headerStyle: HeaderStyle(
+          formatButtonVisible: true,
+          titleCentered: true,
+          formatButtonShowsNext: false,
+          formatButtonDecoration: BoxDecoration(
+            color: Colors.blue,
+            borderRadius: BorderRadius.circular(5.0),
+          ),
+          formatButtonTextStyle: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+}
